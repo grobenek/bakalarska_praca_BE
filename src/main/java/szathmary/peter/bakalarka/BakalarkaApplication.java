427b5f4048bd0914588be3e8008b41d51c1a13fb
Peter Szathmary <peter.szathmary01@gmail.com>
2023-02-20 12:42:19 +0100

    initial commit

---
+++ src/main/java/szathmary/peter/bakalarka/BakalarkaApplication.java
@@ -0,0 +1,13 @@
+package szathmary.peter.bakalarka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakalarkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakalarkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/szathmary/peter/bakalarka/BakalarkaApplicationTests.java
@@ -0,0 +1,13 @@
+package szathmary.peter.bakalarka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakalarkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
